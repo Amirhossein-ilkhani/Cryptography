612a433f9725f91ed2737faf9c32f68ca727ff57
alirezasgh <s.a.saghaeian@gmail.com>
2023-10-17 09:01:25 +0330

    Add N value to output

--- C++/openssl demo/TextOutput/RSA/main.cpp
+++ C++/openssl demo/TextOutput/RSA/main.cpp
@@ -4,42 +4,50 @@
 #include <openssl/pem.h>
 #include <openssl/err.h>
 
-int main() {
+int main()
+{
     OpenSSL_add_all_algorithms();
     ERR_load_crypto_strings();
 
     // Generate an RSA key pair
-    RSA* rsa_keypair = RSA_generate_key(2048, RSA_F4, nullptr, nullptr); //Generates Key pair
-    if (!rsa_keypair) {
+    RSA *rsa_keypair = RSA_generate_key(2048, RSA_F4, nullptr, nullptr); // Generates Key pair
+    if (!rsa_keypair)
+    {
         std::cerr << "Failed to generate RSA key pair." << std::endl;
         return 1;
     }
 
     // Public key and private key
-    BIO* bio_pub = BIO_new(BIO_s_mem());
-    BIO* bio_priv = BIO_new(BIO_s_mem());
+    const BIGNUM *n = BN_new();
+    RSA_get0_key(rsa_keypair, &n, nullptr, nullptr);
+    BIO *bio_pub = BIO_new(BIO_s_mem());
+    BIO *bio_priv = BIO_new(BIO_s_mem());
 
-    PEM_write_bio_RSAPublicKey(bio_pub, rsa_keypair); //Assigns public key to bio
-    PEM_write_bio_RSAPrivateKey(bio_priv, rsa_keypair, nullptr, nullptr, 0, nullptr, nullptr); //Assigns private key to bio
-
-    char* pub_key_data;
-    char* priv_key_data;
+    PEM_write_bio_RSAPublicKey(bio_pub, rsa_keypair);                                          // Assigns public key to bio
+    PEM_write_bio_RSAPrivateKey(bio_priv, rsa_keypair, nullptr, nullptr, 0, nullptr, nullptr); // Assigns private key to bio
+    char *n_hex = BN_bn2hex(n);
+    char *pub_key_data;
+    char *priv_key_data;
     long pub_key_len = BIO_get_mem_data(bio_pub, &pub_key_data);
     long priv_key_len = BIO_get_mem_data(bio_priv, &priv_key_data);
 
     // Print public and private keys
-    std::cout << "Public Key:" << std::endl << pub_key_data << std::endl;
-    std::cout << "Private Key:" << std::endl << priv_key_data << std::endl;
+    std::cout << "Public Key:" << std::endl
+              << pub_key_data << std::endl;
+    std::cout << "Private Key:" << std::endl
+              << priv_key_data << std::endl;
+    std::cout << "Value of N (modulus): " << n << std::endl;
 
     // Encrypt and decrypt a message
-    const char* message = "Hello, RSA!";
+    const char *message = "Hello, RSA!";
     unsigned char encrypted[256]; // Sufficient buffer for a 2048-bit RSA key
     unsigned char decrypted[256];
 
-    int encrypted_len = RSA_public_encrypt(strlen(message), (const unsigned char*)message, encrypted, rsa_keypair, RSA_PKCS1_PADDING);
+    int encrypted_len = RSA_public_encrypt(strlen(message), (const unsigned char *)message, encrypted, rsa_keypair, RSA_PKCS1_PADDING);
     int decrypted_len = RSA_private_decrypt(encrypted_len, encrypted, decrypted, rsa_keypair, RSA_PKCS1_PADDING);
 
-    if (encrypted_len == -1 || decrypted_len == -1) {
+    if (encrypted_len == -1 || decrypted_len == -1)
+    {
         std::cerr << "Encryption or decryption failed." << std::endl;
         return 1;
     }
@@ -50,6 +58,8 @@ int main() {
     RSA_free(rsa_keypair);
     BIO_free_all(bio_pub);
     BIO_free_all(bio_priv);
+    // BN_free(n);
+    OPENSSL_free(n_hex);
 
     ERR_free_strings();
     EVP_cleanup();
